6c63c1bcb82f39b17d0d9478b9965b53fd0f9635
Felipe Mascarin <felipe_mascarin@hotmail.com>
2023-12-28 01:29:05 -0300

    a

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -16,13 +16,7 @@ dotnet tool install --global dotnet-ef --version 6.0.16
 
 export PATH="$PATH:/root/.dotnet/tools"
 
-# Caminho do arquivo de logs do container maislocacoes no linux
-log_file="/root/container.log"
-
-# Cria o arquivo de log no linux se não existir
-touch "$log_file"
-
 # Redireciona a saída padrão e de erro para o arquivo de log
-exec > >(tee -a "$log_file") 2>&1
+exec > >(tee -a "/root/container.log") 2>&1
 
 exec "$@"
