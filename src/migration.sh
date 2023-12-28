91e8fd2adc164ef2cadc75828bcf8ffac4b6241f
Felipe Mascarin <felipe_mascarin@hotmail.com>
2023-12-28 00:49:58 -0300

    a

---
+++ src/migration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+docker exec -it maislocacoes bash
+
+cd MaisLocacoes.WebApi
+
+# Obter a data UTC atual no formato YYYYMMDDHHMMSS
+migration_name=$(date -u +"%Y%m%d%H%M%S")
+
+dotnet ef migrations add $migration_name -c DataBaseContextAdm;
+dotnet ef migrations add $migration_name -c DataBaseContext1;
+dotnet ef migrations add $migration_name -c DataBaseContext2;
+dotnet ef migrations add $migration_name -c DataBaseContext3;
+
+dotnet ef database update -c DataBaseContextAdm;
+dotnet ef database update -c DataBaseContext1;
+dotnet ef database update -c DataBaseContext2;
+dotnet ef database update -c DataBaseContext3;
