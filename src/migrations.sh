d1ad428018f757d9923ba90af350249edcad4918
Felipe Mascarin <felipe_mascarin@hotmail.com>
2024-01-09 01:33:33 -0300

    a

--- src/migrations.sh
+++ src/migrations.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Caminho da pasta no sistema Linux
+source_folder="/root/Migrations"
+
+# Verifica se a pasta existe
+if [ ! -d "$source_folder" ]; then
+  echo "A pasta $source_folder de backup não existe no linux. A pasta deve sempre ser a atualizada. O script será encerrado."
+  exit 1
+fi
+
+# Caminho dentro do contêiner
+container_name="container-maislocacoes"
+container_folder="/app/Migrations"
+
+# Copia a pasta para dentro do contêiner no caminho /app
+docker cp "$source_folder" "$container_name":"$container_folder"
+
 # Obtenha a data/hora UTC atual no formato YYYYMMDD_HHMMSS
 current_datetime=$(date -u +"%Y%m%d_%H%M%S")
 
@@ -12,8 +28,16 @@ dotnet ef migrations add ${migration_name} -c DataBaseContext1;
 dotnet ef migrations add ${migration_name} -c DataBaseContext2;
 dotnet ef migrations add ${migration_name} -c DataBaseContext3;
 
-# Atualiza os bancos de dados
+# Atualiza todos os bancos de dados
 dotnet ef database update -c DataBaseContextAdm;
 dotnet ef database update -c DataBaseContext1;
 dotnet ef database update -c DataBaseContext2;
 dotnet ef database update -c DataBaseContext3;
+
+# Copia a pasta da migration atualizada de volta pro linux para backup:
+# Salva com nome temporatio
+mv "$container_folder" "$source_folder"_temp
+# Deleta a pasta antiga
+rm -rf "$source_folder"
+# Renomeia a de nome temporário para o nome Migrations
+mv "$source_folder"_temp "$source_folder"
