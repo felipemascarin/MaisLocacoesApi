b20c1f7a81c02cd322a2dd4a93cbe3bb95430fdb
Felipe Mascarin <felipe_mascarin@hotmail.com>
2024-03-06 20:18:28 -0300

    a

--- src/MaisLocacoes.WebApi/Domain/Models/v1/Response/Bill/GetBillByRentIdResponse.cs
+++ src/MaisLocacoes.WebApi/Domain/Models/v1/Response/Bill/GetBillByRentIdResponse.cs
@@ -28,7 +28,6 @@
             public string Type { get; set; }
             public bool IsManyParts { get; set; }
             public string CreatedBy { get; set; }
-            public string LastCreatedCode { get; set; }
             public DateTime CreatedAt { get; set; }
             public string UpdatedBy { get; set; }
             public DateTime? UpdatedAt { get; set; }
